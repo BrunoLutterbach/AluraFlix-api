7b4f894e57ab49ae1bc8d8d7fbd50c2166f38f72
Bruno <brunolutterbach13@gmail.com>
2022-11-28 20:29:05 -0300

    Handler para id não existente quando deletar

--- src/main/java/br/com/brunolutterbach/aluraflix/exception/RestExceptionHandler.java
+++ src/main/java/br/com/brunolutterbach/aluraflix/exception/RestExceptionHandler.java
@@ -1,7 +1,10 @@
 package br.com.brunolutterbach.aluraflix.exception;
 
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,6 +19,7 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
+    @ResponseStatus(code = BAD_REQUEST)
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException e) {
         ResourceNotFoundDetails resourceNotFoundDetails = ResourceNotFoundDetails.builder()
@@ -31,7 +35,7 @@ public class RestExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ValidationErrorDetails handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         List<String> errors = e.getBindingResult().getFieldErrors().stream()
-                .map(fieldError -> fieldError.getField()).toList();
+                .map(FieldError::getField).toList();
         return ValidationErrorDetails.builder()
                 .title("Erro de validação")
                 .date(ZonedDateTime.now())
@@ -39,4 +43,28 @@ public class RestExceptionHandler {
                 .detail("Erro no campo " + errors + ". " + Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage())
                 .build();
     }
+
+    @ResponseStatus(code = BAD_REQUEST)
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
+        ResourceNotFoundDetails resourceNotFoundDetails = ResourceNotFoundDetails.builder()
+                .title("Erro")
+                .date(ZonedDateTime.now())
+                .status(404)
+                .detail(e.getMessage())
+                .build();
+        return new ResponseEntity<>(resourceNotFoundDetails, HttpStatus.valueOf(resourceNotFoundDetails.getStatus()));
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<?> handleEmptyResultDataAccessException() {
+        ResourceNotFoundDetails resourceNotFoundDetails = ResourceNotFoundDetails.builder()
+                .title("Erro")
+                .date(ZonedDateTime.now())
+                .status(404)
+                .detail("Id informado para deletar não existe")
+                .build();
+        return new ResponseEntity<>(resourceNotFoundDetails, HttpStatus.valueOf(resourceNotFoundDetails.getStatus()));
+    }
+
 }
